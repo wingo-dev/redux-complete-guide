035b666ca920ba4409cdec5ee4d5a5ea168492aa
wingo-dev <rukshanlancer@gmail.com>
2023-03-28 17:03:07 +0800

    we can use redux in any javascript projects

--- redux-demo.js
+++ redux-demo.js
@@ -1,15 +1,26 @@
 const redux = require('redux');
 
 const counterReducer = (state = {counter:0}, action) =>{
-    return {
-        counter: state.counter + 1
-
-    };
+    if(action.type === 'increment')
+    {
+        return {
+            counter: state.counter + 1
+    
+        };
+    }
+    if(action.type === 'decrement')
+    {
+        return{
+            counter: state.counter-1
+        };
+    }
+    return state;
+    
 };
 
 const store = redux.createStore(counterReducer);
 
-console.log(store.getState());
+// console.log(store.getState());
 
 const counterSubscriber = () => {
     const latestState = store.getState();
@@ -19,3 +30,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({type: 'increment'});
+store.dispatch({type: 'decrement'});
